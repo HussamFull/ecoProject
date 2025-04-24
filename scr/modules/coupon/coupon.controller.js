2bd5870811084a42199f989dc47d917915c04876
hamada <bhak6150@gmail.com>
2025-04-24 03:44:06 +0200

    get coupon by id+  update coupon by id + delete coupon by id

--- scr/modules/coupon/coupon.controller.js
+++ scr/modules/coupon/coupon.controller.js
@@ -13,3 +13,33 @@ export const create = async (req, res) => {
    const coupon = await couponModel.create( req.body);
     return res.status(201).json({ message: "Coupon created successfully!", coupon });
 }
+// get all coupons
+export const get = async (req, res) => {
+    const coupons = await couponModel.find();
+    return res.status(200).json({ message: "Coupons retrieved successfully!", coupons });
+}
+// get coupon by id
+export const getById = async (req, res) => {
+    const coupon = await couponModel.findById(req.params.id);
+    if (!coupon) {
+        return res.status(404).json({ message: "Coupon not found!" });
+    }
+    return res.status(200).json({ message: "Coupon retrieved successfully!", coupon });
+}
+// update coupon by id
+export const update = async (req, res) => {
+    const coupon = await couponModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!coupon) {
+        return res.status(404).json({ message: "Coupon not found!" });
+    }       
+    return res.status(200).json({ message: "Coupon updated successfully!", coupon });
+}   
+// delete coupon by id
+export const deleteCoupon = async (req, res) => {
+    const coupon = await couponModel.findByIdAndDelete(req.params.id);
+    if (!coupon) {
+        return res.status(404).json({ message: "Coupon not found!" });
+    }       
+    return res.status(200).json({ message: "Coupon deleted successfully!", coupon });
+}
+
--- scr/modules/coupon/coupon.router.js
+++ scr/modules/coupon/coupon.router.js
@@ -5,6 +5,10 @@ const router = Router();
 
 // Routes 
 router.post('/',auth(['admin']), controller.create);
+router.get('/',auth(['admin']) , controller.get);
+router.get('/:id',auth(['admin']) , controller.getById);
+router.put('/:id',auth(['admin']) , controller.update);
+router.delete('/:id',auth(['admin']) , controller.deleteCoupon);
 
 
 export default router;
